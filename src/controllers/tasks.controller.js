40ea6fa438c1b16201d8f02e7a87a19978f9120e
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-10-26 14:53:38 -0500

    middleware para capturar el error

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,18 +1,15 @@
 const pool = require('../db');
 
-const getAllTasks = async (req, res) => {
+const getAllTasks = async (req, res, next) => {
   try {
     const allTasks = await pool.query('SELECT * FROM task');
     res.json(allTasks.rows)
   } catch (error) {
-    res.json({
-      error: error.message,
-      stack: error.stack
-    });
+    next(error)
   }
 }
 
-const getTask = async (req, res) => {
+const getTask = async (req, res, next) => {
   try {
     const { id } = req.params;
 
@@ -26,14 +23,11 @@ const getTask = async (req, res) => {
 
     return res.json(result.rows[0]);
   } catch (error) {
-    res.json({
-      error: error.message,
-      stack: error.stack
-    });
+    next(error)
   }
 }
 
-const createTask = async (req, res) => {
+const createTask = async (req, res, next) => {
   const { title, description } = req.body;
 
   try {
@@ -44,31 +38,32 @@ const createTask = async (req, res) => {
 
     res.json(result.rows[0]);
   } catch (error) {
-    res.json({
-      error: error.message,
-      stack: error.stack
-    });
+    next(error)
+  };
+};
+
+const updateTask = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { title, description } = req.body;
+  
+    const result = await pool.query('UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *', [title, description, id]);
+  
+    if (!result.rows.length) {
+      return res.status(404).json({
+        message: 'Task not found'
+      });
+    }
+  
+    console.log(result);
+  
+    return res.json(result.rows[0])
+  } catch (error) {
+    next(error)
   }
 }
 
-const updateTask = async (req, res) => {
-  const { id } = req.params;
-  const { title, description } = req.body;
-
-  const result = await pool.query('UPDATE task SET title = $1, description = $2 WHERE id = $3 RETURNING *', [title, description, id]);
-
-  if (!result.rows.length) {
-    return res.status(404).json({
-      message: 'Task not found'
-    });
-  }
-
-  console.log(result);
-
-  return res.json(result.rows[0])
-}
-
-const deleteTask = async (req, res) => {
+const deleteTask = async (req, res, next) => {
   try {
     const { id } = req.params;
 
@@ -82,10 +77,7 @@ const deleteTask = async (req, res) => {
 
     return res.sendStatus(204);
   } catch (error) {
-    res.json({
-      error: error.message,
-      stack: error.stack
-    });
+    next(error)
   }
 }
 
