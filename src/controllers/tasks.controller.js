ad29d4c2d1896ea8dab465f11b1143e7ded1105c
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-10-26 13:50:23 -0500

    retornar un unico dato por id

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -12,8 +12,25 @@ const getAllTasks = async (req, res) => {
   }
 }
 
-const getTask = (req, res) => {
-  res.send('Estoy retornando una tarea');
+const getTask = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const result = await pool.query('SELECT * FROM task WHERE id = $1', [id])
+
+    if (!result.rows.length) {
+      return res.status(404).json({
+        message: 'Task not found'
+      });
+    }
+
+    return res.json(result.rows[0]);
+  } catch (error) {
+    res.json({
+      error: error.message,
+      stack: error.stack
+    });
+  }
 }
 
 const createTask = async (req, res) => {
