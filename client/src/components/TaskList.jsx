aab9914ddccb420ae66909eb8f2ac0be58a57424
Sneyder <sneyder.h.rodriguez@gmail.com>
2022-10-28 12:03:08 -0500

    retornando la lista de datos y proporcionando estilos con material ui

--- client/src/components/TaskList.jsx
+++ client/src/components/TaskList.jsx
@@ -1,5 +1,61 @@
+import { useEffect, useState } from 'react';
+import { Button, Card, CardContent, Typography } from '@mui/material';
+
 export default function TaskList() {
+
+  const [tasks, setTasks] = useState([]);
+
+  const returnTask = async () => {
+    const response = await fetch('http://localhost:4000/tasks')
+    const data = await response.json()
+    setTasks(data)
+  }
+
+  useEffect(() => {
+    returnTask()
+  }, [])
+
   return (
-    <div>TaskList</div>
+    <>
+      <h1>Lista de Tasks</h1>
+      {
+        tasks.map(task => (
+          <Card key={task.id} style={{
+            marginBottom: '.7rem',
+            backgroundColor: '#1e272e'
+          }}>
+            <CardContent style={{
+              display: 'flex',
+              justifyContent: 'space-between'
+            }}>
+              <div style={{color: 'white'}}>
+                <Typography>{task.title}</Typography>
+                <Typography>{task.description}</Typography>
+              </div>
+
+              <div>
+                <Button
+                  variant='contained'
+                  color='inherit'
+                  onClick={() => console.log('Editando')}
+                >
+                  Edit
+                </Button>
+                <Button
+                  variant='contained'
+                  color='warning'
+                  onClick={() => console.log('Eliminando')}
+                  style={{
+                    margin: '.5rem'
+                  }}
+                >
+                  Delete
+                </Button>
+              </div>
+            </CardContent>
+          </Card>
+        ))
+      }
+    </>
   )
 }
